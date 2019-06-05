219c8139f295ee7fdebe66f05f2d975c794cc1db
Martin Rey <mrey@suse.com>
2019-06-05 20:09:11 +0200

    Change list parsing from json to text; Remove commented-out code; Add get_rpmlint_error_list function

--- filerpmlintbug.py
+++ filerpmlintbug.py
@@ -1,11 +1,11 @@
 import argparse
 import bugzilla
 from configparser import ConfigParser
-import json
 from os.path import join
-import requests
 import sys
 import urllib.parse
+import urllib.request
+import json
 
 __author__ = "Martin Rey <mrey@suse.de>"
 
@@ -19,22 +19,21 @@ def bugzilla_init(apiurl, username, password):
     return bugzilla_api
 
 
-def get_rpmlint_json(urlrpmlint, project, arch, repo, rpmlint_error):
-    rpmlint_path = join(project, arch, repo)
-    rpmlint_url_split = urllib.parse.urlsplit(urlrpmlint)
-    rpmlint_url = urllib.parse.urlunsplit((rpmlint_url_split.scheme, rpmlint_url_split.netloc, rpmlint_path,
-                                           "rule=" + rpmlint_error + "&format=json", ""))
-    rpmlint_json_text = requests.get(rpmlint_url).text
-    try:
-        rpmlint_json = json.loads(rpmlint_json_text)
-    except json.decoder.JSONDecodeError as e:
-        print("JSONDecodeError: Wrong input params?")
-        sys.exit()
-    return rpmlint_json
+def get_rpmlint_package_list(urlrpmlint, project, arch, repo, rpmlint_error):
+    rpmlint_url = join(urlrpmlint, project, arch, repo + "?rule=" + rpmlint_error + "&format=txt")
+    with urllib.request.urlopen(rpmlint_url) as response:
+        packages = response.read().decode("utf-8").splitlines()
+    return packages
+
+
+def get_rpmlint_error_list(urlrpmlint, project, arch, repo):
+    rpmlint_url = join(urlrpmlint, "rules", project, arch, repo + "?format=txt")
+    with urllib.request.urlopen(rpmlint_url) as response:
+        errors = response.read().decode("utf-8").splitlines()
+    return errors
 
 
 def main(args):
-
     config = ConfigParser()
     if args.config:
         config.read(args.config)
@@ -50,31 +49,22 @@ def main(args):
     if args.urlbugzilla is not None:
         config["Bugzilla_instance"]["url"] = args.urlbugzilla
     if args.username is not None:
-        config["Bugzilla_instance"]["username"] = args.username
+        config["Bugzilla_instance"]["login_username"] = args.username
     if args.password is not None:
-        config["Bugzilla_instance"]["password"] = args.password
+        config["Bugzilla_instance"]["login_password"] = args.password
     if args.bug is not None:
         config["Bugzilla_bug"]["blocks"] = str(args.bug)
 
-    rpmlint_json = get_rpmlint_json(config["BuildCheckStatistics_instance"]["url"],
+    errors = get_rpmlint_error_list(config["BuildCheckStatistics_instance"]["url"],
                                     config["BuildCheckStatistics_instance"]["project"],
                                     config["BuildCheckStatistics_instance"]["architecture"],
-                                    config["BuildCheckStatistics_instance"]["repository"],
-                                    args.rpmlint_error)
-    packages = []
-    for p in rpmlint_json:
-        packages.append(p["package"])
-    print(packages)
+                                    config["BuildCheckStatistics_instance"]["repository"])
 
-    bugzilla_api = bugzilla_init(config["Bugzilla_instance"]["url"],
-                                 config["Bugzilla_instance"]["username"],
-                                 config["Bugzilla_instance"]["password"])
-    parent_bug = bugzilla_api.getbug(config["Bugzilla_bug"]["blocks"])
-    print(parent_bug)
-
-    # for package in packages:
-    #     data_child_bug = bugzilla_api.build_createbug(**dict(config.items("Bugzilla_bug")))
-    #     bugzilla_api.createbug(data_child_bug)
+    packages = get_rpmlint_package_list(config["BuildCheckStatistics_instance"]["url"],
+                                        config["BuildCheckStatistics_instance"]["project"],
+                                        config["BuildCheckStatistics_instance"]["architecture"],
+                                        config["BuildCheckStatistics_instance"]["repository"],
+                                        args.rpmlint_error)
 
 if __name__ == '__main__':
     # file-rpmlint-bug --urlrpmlint https://rpmlint.opensuse.org --project openSUSE:Factory --arch x86_64 \
